1ac9bdb1862df5824186b2edb24f5c4a89e54afe
nikita-blshkv <nikita.blshkv@gmail.com>
2026-08-24 03:22:30 +0700

    fix: track bybit_keys.py, stop tracking __pycache__

---
+++ core/bybit_keys.py
@@ -0,0 +1,5 @@
+import os
+
+BYBIT_DEMO_API_KEY = os.environ.get("BYBIT_DEMO_API_KEY", "")
+BYBIT_DEMO_API_SECRET = os.environ.get("BYBIT_DEMO_API_SECRET", "")
+BYBIT_DEMO_BASE_URL = "https://api-demo.bybit.com"
